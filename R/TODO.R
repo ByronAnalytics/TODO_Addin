07eb53674fcacdcf9c625a80c9b67f4d67297d32
Kevin Byron <Kwbyron@gmail.com>
2017-01-31 16:08:05 +0100

    Build test clean, documentation refined

--- R/TODO.R
+++ R/TODO.R
@@ -1,14 +1,28 @@
+#' @title Build TODO list from package directory
+#'
+#' @description Addin scans all .R and .Rmd files in the poject directory for
+#'   'TODO' comments TODO items are grouped by file name and ordered by line
+#'   number, then writen to a seperate file: TODO.txt This file is overwritten
+#'   each time the addin is executed.
+#'
+#' @param taxaList A vector of species names to be validated
+#' @param dictionary A vector of valid species names
+#' @param synTable A dataframe with current names paired with previous synonyms
+#'
+#' @return a dataframe of mis-matched names and their suggested taxonomy.
+#' @export
+
 buildTODO <- function(){
-  DIR <- getActiveProject()
+  DIR <- rstudioapi::getActiveProject()
 
   rFiles <- list.files(path = DIR, pattern = "(?i)\\.r$", recursive = TRUE)
   rmdFiles <- list.files(path = DIR, pattern = "(?i)\\.rmd$", recursive = TRUE)
   rFiles <- c(rFiles, rmdFiles)
 
-  response <- tkmessageBox(message = "Scan project directory for TODO Items (this may take a moment)?",
+  response <- tcltk::tkmessageBox(message = "Scan project directory for TODO Items (this may take a moment)?",
                icon = "question", type = "yesnocancel", default = "yes")
-  if(tclvalue(response) == "yes"){
-    allTODOs <- lapply(rFiles, GetTODOs, DIR = DIR)
+  if(tcltk::tclvalue(response) == "yes"){
+    allTODOs <- lapply(rFiles, .GetTODOs, DIR = DIR)
     allTODOs<- do.call(c,allTODOs)
 
     cat(allTODOs, file="TODO.txt")
@@ -16,21 +30,22 @@ buildTODO <- function(){
     msgBox <- tcltk::tkmessageBox(title = "ALERT!",
                                   message = "TODO List built!\n Would you like to open the file?'",
                                   icon = "question", type = "yesno")
-    if(tclvalue(msgBox) == "yes"){
+    if(tcltk::tclvalue(msgBox) == "yes"){
       toOpen <- paste(DIR, "TODO.txt", sep = "/")
       if(file.exists(toOpen)){
-        navigateToFile(toOpen)
+        rstudioapi::navigateToFile(toOpen)
       } else {
-        tkmessageBox(message = "File wasn't found", icon = "error", type = "ok")
+        tcltk::tkmessageBox(message = "File wasn't found", icon = "error", type = "ok")
       }
     }
   } else {
-    tkmessageBox(message = "Addin was canceled", icon = "error", type = "ok")
+    tcltk::tkmessageBox(message = "Addin was canceled", icon = "error", type = "ok")
   }
 
 }
 
-GetTODOs <- function(FILE, DIR){
+
+.GetTODOs <- function(FILE, DIR){
   # Scan R code as character vector, line by line.
   code <- scan(file=paste(DIR, FILE, sep = "\\"), what = "character", sep = "\n", blank.lines.skip = FALSE)
 
